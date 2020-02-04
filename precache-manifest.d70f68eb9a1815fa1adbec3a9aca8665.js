c2816a2f1476b7e9c688115e760e9a6bbd39aadd
Matthew <m-semenets@mail.ru>
2020-02-04 20:28:41 +0300

    Updates

---
+++ precache-manifest.d70f68eb9a1815fa1adbec3a9aca8665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb6f100d9c1205ef6855bf9dd1e4c17",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "bd9aac7922f9354ca132",
+    "url": "/blog/static/css/main.0365523d.chunk.css"
+  },
+  {
+    "revision": "78b5a5217d191c9afbfe",
+    "url": "/blog/static/js/2.fca6c345.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/blog/static/js/2.fca6c345.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd9aac7922f9354ca132",
+    "url": "/blog/static/js/main.cbf3ba2a.chunk.js"
+  },
+  {
+    "revision": "cbfd563c8600f0c401a0",
+    "url": "/blog/static/js/runtime-main.3e812e36.js"
+  }
+]);
